e944b8e1e9b9710cf4a766f3b22c9c31815b98b2
Mudit <srivastava.mudit2908@gmail.com>
2016-05-07 00:58:18 +0530

    Add GridView

---
+++ project/app/src/main/java/com/example/android/popularmoviesstage1/app/MovieItem.java
@@ -0,0 +1,12 @@
+package com.example.android.popularmoviesstage1.app;
+
+/**
+ * Created by DELL on 5/6/2016.
+ */
+public class MovieItem {
+    int image;
+    public MovieItem(int image) {
+        this.image = image;
+    }
+
+}
